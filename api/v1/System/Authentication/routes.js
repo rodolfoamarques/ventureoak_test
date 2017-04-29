fe434d021776894ba20397f5b48b2ef547d74534
Rodolfo Marques <rodolfoalmeidamarques@gmail.com>
2017-04-29 23:40:42 +0100

    Upgraded Lab version

--- api/v1/System/Authentication/routes.js
+++ api/v1/System/Authentication/routes.js
@@ -8,47 +8,41 @@ const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"
 
 
 module.exports = [
-{
-  method: 'POST',
-  path: `/${endpoint}/login`,
-  handler: controller.login,
-  config: {
-    description: 'Login',
-    notes: 'Login',
-    tags: ['api', 'v1', 'authentication'],
-    auth: false,
-    validate: {
-      payload:
-        joi.object({
-          email: joi.string().email().regex(email_regex).required().description('User\'s email'),
-          password: joi.string().required().description('User\'s password'),
-        }).meta({ className: 'loginModel' }).description('Login form')
+  {
+    method: 'POST',
+    path: `/${endpoint}/login`,
+    handler: controller.login,
+    config: {
+      description: 'Login',
+      notes: 'Login',
+      tags: ['api', 'v1', 'authentication'],
+      auth: false,
+      validate: {
+        payload:
+          joi.object({
+            email: joi.string().email().regex(email_regex).required().description('User\'s email'),
+            password: joi.string().required().description('User\'s password'),
+          }).meta({ className: 'loginModel' }).description('Login form')
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: `/${endpoint}/register`,
+    handler: controller.register,
+    config: {
+      description: 'Registration of a new user',
+      notes: 'Registration of a new user',
+      tags: ['api', 'v1', 'authentication'],
+      auth: false,
+      validate: {
+        payload:
+          joi.object({
+            email: joi.string().email().regex(email_regex).required().description('User\'s login email'),
+            password: joi.string().required().description('User\'s login password'),
+            password_confirmation: joi.string().required().description('Password confirmation'),
+            full_name: joi.string().description('User\'s real full name'),
+          }).meta({ className: 'registrationModel' }).description('Register new user form')
     }
   }
-}, {
-  method: 'POST',
-  path: `/${endpoint}/register`,
-  handler: controller.register,
-  config: {
-    description: 'Registration of a new user',
-    notes: 'Registration of a new user',
-    tags: ['api', 'v1', 'authentication'],
-    auth: false,
-    validate: {
-      payload:
-        joi.object({
-          id: joi.any().forbidden(),
-          email: joi.string().email().regex(email_regex).required().description('User\'s login email'),
-          password: joi.string().required().description('User\'s login password'),
-          password_confirmation: joi.string().required().description('Password confirmation'),
-          full_name: joi.string().description('User\'s real full name'),
-          role_id: joi.any().forbidden(),
-          enabled: joi.any().forbidden(),
-          last_login_at: joi.any().forbidden(),
-          created_at: joi.any().forbidden(),
-          updated_at: joi.any().forbidden(),
-          deleted_at: joi.any().forbidden()
-        }).meta({ className: 'registrationModel' }).description('Register new user form')
-    }
-  }
-}];
+];
